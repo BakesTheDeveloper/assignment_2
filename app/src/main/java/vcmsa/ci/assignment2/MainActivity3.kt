df0d843f13c8c4e06406804eb31396823612592b
BakesTheDeveloper <ST10469661@vcconnect.edu.za>
2025-05-20 22:57:16 +0200

    alot.

--- app/src/main/java/vcmsa/ci/assignment2/MainActivity3.kt
+++ app/src/main/java/vcmsa/ci/assignment2/MainActivity3.kt
@@ -1,6 +1,7 @@
 package vcmsa.ci.assignment2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,17 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+
+        val score = intent.getIntExtra("SCORE", 0)
+        val total = intent.getIntExtra("TOTAL", 0)
+
+        val ScoreText = findViewById<TextView>(R.id.FinalScoreView)
+        ScoreText.text = "Your score: $score / $total"
+
+        val ExitButton = findViewById<Button>(R.id.exitbutton)
+        ExitButton.setOnClickListener{
+            finish()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
