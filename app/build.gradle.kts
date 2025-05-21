7b78ec3beef29ff5c924e3f2cb0552260d8d08be
BakesTheDeveloper <Bakes@MacBook-Air.local>
2025-05-21 23:43:34 +0200

    fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "vcmsa.ci.assignment2"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "vcmsa.ci.assignment2"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
